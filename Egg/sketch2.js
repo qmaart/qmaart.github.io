19def33c88c5aaf945a88a9faf61a7f0c7fdf1dc
ElaineM <maelaine27@gmail.com>
2019-05-07 13:54:18 -0700

    image file name change

--- Egg/sketch2.js
+++ Egg/sketch2.js
@@ -13,7 +13,7 @@ function preload() {
   img = loadImage('image/pan.jpg');
   //create an animation from a sequence of numbered images
   //pass the first and the last file name and it will try to find the ones in between
-  egg = loadAnimation('image/eggflip（front）00.png', 'image/eggflip（front）30.png');
+  egg = loadAnimation('image/egg00.png', 'image/egg30.png');
   // img1 = loadImage('eggflipJPG/eggflip（front）00.png');
   //create an animation listing all the images files
   egg.looping = false;
