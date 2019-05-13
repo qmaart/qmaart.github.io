d202c4157febf0d71c24988f5e45e07f25936d09
ElaineM <maelaine27@gmail.com>
2019-05-12 17:05:01 -0700

    new edit

--- Egg/sketch1.js
+++ Egg/sketch1.js
@@ -2,16 +2,18 @@
 //click on the canvas to create self destructing sprite and toggle visibility
 
 var egg;
-let img, img1,img2;
+let img, img1,img2,img3,img4;
 var count=1;
-var henY=-1100;
+var henY=-1500;
 
 
 function preload() {
   //background image
-  img = loadImage('image/pan.jpg');
+  img = loadImage('image/pan.png');
   img1 = loadImage('image/hen.png');
   img2 = loadImage('image/crack.png')
+  // img3 = loadImage('image/background.png')
+  img4 = loadImage('image/hint.png')
 }
 
 function setup() {
@@ -20,11 +22,15 @@ function setup() {
   egg = createSprite(width/2, height/2);
   egg.addAnimation('normal', 'image/egg00.png', 'image/egg01.png');
   egg.velocity.x = 3;
+  egg.scale=1.5;
+
 }
 
 function draw() {
-  background(255, 255, 255);
+  background(247, 217, 200);
+  // image(img3,0,0,width,height);
   image(img,0,0,width,height);
+  image(img4,0,0,width,height);
   //sprites' visibility can be turned on an off
   //and invisible sprite is still updating normally
 
@@ -55,8 +61,26 @@ function mousePressed() {
   flippingegg.addAnimation('normal', 'image/egg00.png', 'image/egg30.png');
   //set a self destruction timer (life)
   flippingegg.life = 120;
+  flippingegg.scale=1.5
   if (count>5){
   flippingegg.visible=false;
   // img(img2,random(),random(),200,200);
   }
+}
+
+function deviceTurn(){
+  if (turnAxis === 'X'||turnAxis === 'Y') {
+    count++;
+    henY=henY+120;
+    //create a sprite
+    var flippingegg = createSprite(width/2, height/2);
+    flippingegg.addAnimation('normal', 'image/egg00.png', 'image/egg30.png');
+    //set a self destruction timer (life)
+    flippingegg.life = 120;
+    flippingegg.scale=1.5
+    if (count>5){
+    flippingegg.visible=false;
+    // img(img2,random(),random(),200,200);
+    }
+}
 }
